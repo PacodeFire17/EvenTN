9e9cc3818cb4f4cd361c3c9f54deb470421aefc3
Luca Nardon <luca.nardon@unitn.it>
2024-11-11 10:30:15 +0100

    first structure of the backend

---
+++ index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 10000;
+const DB_URL = process.env.DB_URL;
+
+app.use(express.json());
+
+// the list of methods goes here
+
+mongoose.connect(DB_URL).then(()=>{
+
+    app.listen(PORT,()=>{console.log('listening on port: %d', PORT)});
+    
+}).catch((err)=>console.log('Failed to connect to database: %d', DB_URL));

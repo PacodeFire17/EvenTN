3017b475fe331d0970ac2747c491d628c9444e0e
Davideglo <151943873+Davideglo@users.noreply.github.com>
2024-11-15 09:58:03 +0100

    Add files via upload
    show booking list

---
+++ methods/bookingList.js
@@ -0,0 +1,34 @@
+const Event = require('../models/Event');
+const Booking = require('../models/Booking');
+
+// Funzione per ottenere tutti gli ID delle prenotazioni per un evento
+const bookingList = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    // Trova l'evento con l'ID fornito
+    const event = await Event.findById(id);
+
+    if (!event) {
+      return res.status(404).json({ message: 'Event not found.' });
+    }
+
+    // Verifica che l'utente che richiede i dati sia l'organizzatore dell'evento
+    if (event.organizerId.toString() !== req.user.id.toString()) {
+      return res.status(401).json({ message: 'Access denied: you are not allowed to see the booking list of this event.' });
+    }
+
+    // Trova tutte le prenotazioni associate a questo evento
+    const reservations = await Booking.find({ event: id }).select('id');
+
+    // Restituisci gli ID delle prenotazioni
+    const reservationIds = reservations.map(reservation => reservation._id);
+    
+    res.status(200).json({ reservationIds });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server internal error.' });
+  }
+};
+
+module.exports = { bookingList };
